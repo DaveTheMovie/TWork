8c88fef148cce337e6bf966f7671fdefd521ef17
DaveTheMovie <davethemovie@gmail.com>
2020-11-08 16:22:23 -0500

    first commit

---
+++ test_MySum.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  8 16:19:22 2020
+
+@author: david
+"""
+
+
+from MySum import MySum
+
+
+test_MySum():
+    assert(Mysum(1,2)==3)
